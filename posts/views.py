c331a15f82132bd39b313b2b7c0ace1f4007d872
YoohwaJin <qudghkehdtod@naver.com>
2023-08-30 10:50:47 +0900

    image

---
+++ posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
+
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = PostForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'form.html', context)
